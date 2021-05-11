a2e674da1f670946895ae8d02d301e0c738fe84d
Logta <takenobu15@gmail.com>
2021-05-11 19:56:58 +0900

    fix fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,35 @@
 import "../styles/globals.css";
 import { GlobalContainer } from "../src/components/containers";
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "Noto Sans JP",
+      "Lato",
+      "游ゴシック Medium",
+      "游ゴシック体",
+      "Yu Gothic Medium",
+      "YuGothic",
+      "ヒラギノ角ゴ ProN",
+      "Hiragino Kaku Gothic ProN",
+      "メイリオ",
+      "Meiryo",
+      "ＭＳ Ｐゴシック",
+      "MS PGothic",
+      "sans-serif",
+    ].join(","),
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <GlobalContainer>
-      <Component {...pageProps} />
-    </GlobalContainer>
+    <ThemeProvider theme={theme}>
+      <GlobalContainer>
+        <Component {...pageProps} />
+      </GlobalContainer>
+    </ThemeProvider>
   );
 }
 
