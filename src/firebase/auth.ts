d6a06514b074fa17cdef2437532a13e6e53b30f8
logta <takenobu15@gmail.com>
2022-03-05 22:21:40 +0900

    fix awaitOnGoogleLogin

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,6 +1,8 @@
 import { auth, firebase } from "@/plugins/firebase";
 import { Auth } from "@/models";
 
+import {  signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 const awaitOnAuth = async (): Promise<Auth> => {
   return new Promise(function (resolve, reject) {
     auth.onAuthStateChanged((user: any) => {
@@ -22,27 +24,36 @@ const awaitOnAuth = async (): Promise<Auth> => {
 };
 
 const awaitOnGoogleLogin = async (): Promise<Auth> => {
-  const provider = new firebase.auth.GoogleAuthProvider();
+  const provider = new GoogleAuthProvider();
+
   return new Promise(function (resolve, reject) {
-    auth
-      .signInWithPopup(provider)
+    signInWithPopup(auth, provider)
       .then((result) => {
-        var user = result.user;
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
         resolve({
           ok: true,
           id: user.uid,
           name: user.displayName,
         });
-      })
-      .catch((_) => {
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
         reject({
           ok: false,
           id: "",
           name: "",
         });
       });
-  });
-};
+  })
+}
 
 const awaitOnPasswordLogin = async (data: any): Promise<Auth> => {
   const res = await firebase
@@ -64,6 +75,7 @@ const awaitOnPasswordLogin = async (data: any): Promise<Auth> => {
     };
   }
 };
+
 const signOutFirebase = async (): Promise<Auth> => {
   return new Promise(function (resolve, reject) {
     auth.onAuthStateChanged((user) => {
